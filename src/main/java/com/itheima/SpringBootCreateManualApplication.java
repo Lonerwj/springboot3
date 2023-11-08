66fe904c733f854d99ebf3ab2aa3451f22cb17a1
lonerW <1104850148@qq.com>
2023-11-08 15:34:23 +0800

    create project

---
+++ src/main/java/com/itheima/SpringBootCreateManualApplication.java
@@ -0,0 +1,18 @@
+package com.itheima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+
+@SpringBootApplication
+public class SpringBootCreateManualApplication
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(SpringBootApplication.class,args);
+    }
+}
